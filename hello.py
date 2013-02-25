f74cf0eeae1eddf879c1257003b90c836c7389df
Peter.Mueller <Peter.Mueller108@gmx.de>
2013-02-25 06:09:30 +0100

    first committinted

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/lib/python2.7
+import re, string, sys 
+
+print 'Hallo Welt!'
+
+greeting = sys.argv[1] 
+addressee = sys.argv[2] 
+punctuation = sys.argv[3] 
+
+print (int(greeting)+1)
+print sys.argv[0] 
+
